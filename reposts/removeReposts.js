7443899fef6fdac75a97bcafabfb3ab1f61d064a
itamar <itamar4686@gmail.com>
2025-11-29 07:39:52 +0200

    fix: correctly load the data

--- reposts/removeReposts.js
+++ reposts/removeReposts.js
@@ -1,18 +1,21 @@
 const repostSVG = '"M4.75 3.79l4.603 4.3-1.706 1.82L6 8.38v7.37c0 .97.784 1.75 1.75 1.75H13V20H7.75c-2.347 0-4.25-1.9-4.25-4.25V8.38L1.853 9.91.147 8.09l4.603-4.3zm11.5 2.71H11V4h5.25c2.347 0 4.25 1.9 4.25 4.25v7.37l1.647-1.53 1.706 1.82-4.603 4.3-4.603-4.3 1.706-1.82L18 15.62V8.25c0-.97-.784-1.75-1.75-1.75z"'
 
 const deleteReposts = () => {
-    document.querySelectorAll(`path[d=${repostSVG}]`)
+    const timeline = document.querySelector('[aria-label^="Timeline:"]');
+    if (!timeline) return;
+    timeline.querySelectorAll(`path[d=${repostSVG}]`)
         .forEach(el => {
-            el.closest('[data-testid="cellInnerDiv"]').classList.add("hide-repost");
+            el.closest('[data-testid="cellInnerDiv"]').classList.add('hide-repost');
         });
 }
 
 const showReposts = () => {
-    document.querySelectorAll(`[data-testid="cellInnerDiv"]`)
+    const timeline = document.querySelector('[aria-label^="Timeline:"]');
+    if (!timeline) return;
+    timeline.querySelectorAll(`.hide-repost`)
         .forEach(el => {
-            el.classList.remove("hide-repost")
-        }
-        )
+            el.classList.remove('hide-repost')
+        });
 }
 
 const observer = new MutationObserver(() => {
@@ -24,23 +27,23 @@ const main = (enabled) => {
         deleteReposts()
         observer.observe(document, { childList: true, subtree: true });
 
-        console.debug('GIF filter injected');
+        console.debug('Removing reposts');
     } else {
         observer.disconnect()
         showReposts()
-        console.debug('GIF filter disabled - not injecting');
+        console.debug('Returning reposts');
     }
 }
 
-chrome.storage.sync.get('reposts_enabled', ({ reposts_enabled }) => {
-    main(reposts_enabled)
+chrome.storage.sync.get('reposts_enabled', (result) => {
+    console.log(result['reposts_enabled']);
+
+    main(result['reposts_enabled'])
 });
 
 // Listen for changes and reload page
 chrome.storage.onChanged.addListener((changes) => {
     if ('reposts_enabled' in changes) {
         main(changes.reposts_enabled.newValue)
-
-        console.debug('Settings changed - reloading page');
     }
 });
